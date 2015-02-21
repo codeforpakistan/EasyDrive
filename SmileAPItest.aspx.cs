b2c7d5a4bff6e07e25cb4a7f4b5a1326299b5d86
AbdulRehmanDaud <abdulreh_man@hotmail.com>
2015-02-21 19:24:42 +0500

    test file

---
+++ SmileAPItest.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Threading.Tasks;
+using System.Runtime.Serialization.Json;
+using System.Net;
+
+
+public partial class SmileAPItest : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        SMSSession session = GetSession();
+        SendSMS(session, "03335762307", "sample text form easy drive");
+        ALLReciveMessage allmessages = ReciveSMS(session);
+    }
+
+
+}
