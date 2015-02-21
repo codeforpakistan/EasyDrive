fee24145e062454f5acabf72c36d8d90810512c4
umar-qureshi2 <umar.mcs@gmail.com>
2015-02-21 18:08:25 +0500

    new changes
    added forms

---
+++ RegisterSchedule.aspx
@@ -0,0 +1,29 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPage.master" AutoEventWireup="true" CodeFile="RegisterSchedule.aspx.cs" Inherits="RegisterSchedule" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
+    <!--start-contact-->
+    <div class="contact" id="contact">
+        <div class="contact-head">
+            <div class="container">
+                <h3>Welcome to EasyDrive</h3>
+            </div>
+        </div>
+        <div class="contact-content">
+            <div class="container">
+                <div class="col-md-12 contact-part">
+                    <div class="form">
+                        <h3><asp:Literal ID="message" runat="server"></asp:Literal></h3>
+                        <form method="post" action="usersignup.aspx" name="verificationform">
+                            <input type="text" class="textbox" name="verificationcode" required value="Code" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = ' ';}">
+                            <div class="clearfix"></div>
+                            <div class="submit">
+                                <input type="submit" value="Verify" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div class="clear"></div>
+            </div>
+        </div>
+    </div>
+</asp:Content>
---
+++ UserSignup.aspx.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+
+public partial class UserSignup : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        this.message.Text = " Please enter verification code";
+        EasyDrivedbDataContext db = new EasyDrivedbDataContext();
+        if (Request.Form != null)
+        {
+            if (!string.IsNullOrEmpty(Request.Form["signupname"]) && !string.IsNullOrEmpty(Request.Form["signuppassword"]) 
+                && !string.IsNullOrEmpty(Request.Form["signupmobile"]))
+            {
+                this.Session["signupname"] = Request.Form["signupname"];
+                this.Session["verificationcode"] = (new Random(DateTime.Now.Second)).Next(1000, 9999).ToString();
+                Users temp = new Users()
+                {
+                    Name = Request.Form["signupname"],
+                    Mobile = Request.Form["signupmobile"],
+                    Password = Request.Form["signuppassword"],
+                    VerificationCode = (string)(this.Session["verificationcode"])
+                };
+                db.Users.InsertOnSubmit(temp);
+            }
+            else if (!string.IsNullOrEmpty(Request.Form["verificationcode"]))
+            {
+                if (Request.Form["verificationcode"] == Session["verificationcode"])
+                {
+                    var temp = from auser in db.Users
+                                 where auser.Name == (string)Session["signupname"]
+                                 select auser;
+                    foreach (Users person in temp)
+                    {
+                        person.VerificationCode = (-1).ToString();
+                    }
+                }
+                else
+                    this.message.Text = "Verification code mismatch";
+            }
+            try
+            {
+                db.SubmitChanges();
+            }
+            catch (Exception)
+            {
+                
+                message.Text = "DB connection error, please try later or email us at abrehman@hotmail.com";
+            }
+        }
+    }
+}
